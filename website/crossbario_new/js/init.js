47e8ce621d69c02d3059b98c180b20e417fc9e42
Tobias Oberstein <tobias.oberstein@tavendo.de>
2015-08-14 18:49:31 +0200

    hey, hoh!

--- website/crossbario_new/js/init.js
+++ website/crossbario_new/js/init.js
@@ -1,5 +1,6 @@
 (function ($) {
     $(function () {
         console.log("document ready.");
+        $('.button-collapse').sideNav();
     }); // end of document ready
 })(jQuery); // end of jQuery name space
