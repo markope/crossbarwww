be348798415b6542f7315842d38516f4a047490f
Tobias Oberstein <tobias.oberstein@tavendo.de>
2015-08-15 19:47:16 +0200

    sync

--- website/crossbario_new/static/js/site.js
+++ website/crossbario_new/static/js/site.js
@@ -1,6 +1,19 @@
 (function ($) {
+
     $(function () {
         console.log("document ready.");
         $('.button-collapse').sideNav();
     }); // end of document ready
+
+    $(window).scroll(function () {
+        if ($(document).scrollTop() > 0) {
+            $("nav").addClass("nav-scroll-nontop");
+            $("nav").removeClass("nav-scroll-top");
+            //$('body').scrollTo('#main');
+        } else {
+            $("nav").addClass("nav-scroll-top");
+            $("nav").removeClass("nav-scroll-nontop");
+        }
+    });
+
 })(jQuery); // end of jQuery name space
