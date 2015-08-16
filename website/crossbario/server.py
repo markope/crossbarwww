e679703fe00d7c1088d45144a2e54c6e80276062
Tobias Oberstein <tobias.oberstein@tavendo.de>
2015-08-16 23:53:08 +0200

    sync

--- website/crossbario/server.py
+++ website/crossbario/server.py
@@ -10,7 +10,9 @@ from twisted.web.server import Site
 from twisted.web.wsgi import WSGIResource
 
 from flask import Flask, Request, request, session, \
-    g, url_for, abort, render_template, flash
+    g, url_for, abort, render_template, flash, send_from_directory
+
+from markdown import DocPages, get_git_latest_commit
 
 
 # make sure we have MIME types defined for all the
@@ -38,6 +40,16 @@ else:
 #
 app = SiteFlask(__name__)
 app.secret_key = str(uuid.uuid4())
+app.config['DEBUG'] = True
+app.config['CROSSBARDOCS'] = os.path.abspath('../../../crossbardocs')
+
+d = os.path.join(app.config['CROSSBARDOCS'], 'pages/docs')
+print("Processsing Markdown pages from {} ..".format(d))
+app.pages_docs = DocPages(d)
+
+d = os.path.join(app.config['CROSSBARDOCS'], 'pages/iotcookbook')
+print("Processsing Markdown pages from {} ..".format(d))
+app.pages_iotcookbook = DocPages(d)
 
 # Routes
 #
@@ -45,9 +57,47 @@ app.secret_key = str(uuid.uuid4())
 def page_home():
    return render_template('index.html')
 
-@app.route('/docs')
-def page_docs_home():
-   return render_template('docpage.html')
+@app.route('/docs/<path:path>/')
+@app.route('/docs/')
+def page_docs(path=None):
+   if path is None or path.strip() == "":
+      title = 'Documentation'
+      path = 'Home'
+   else:
+      title = path.replace('-', ' ')
+
+   contents = app.pages_docs.render(path)
+
+   if contents:
+      return render_template('page_docs.html', contents=contents, title=title)
+   else:
+      abort(404)
+
+@app.route('/iotcookbook/<path:path>/')
+@app.route('/iotcookbook/')
+def page_iotcookbook(path=None):
+   if path is None or path.strip() == "":
+      title = 'IoT Cookbook'
+      path = 'Home'
+   else:
+      title = path.replace('-', ' ')
+
+   contents = app.pages_iotcookbook.render(path)
+
+   if contents:
+      return render_template('page_iotcookbook.html', contents=contents, title=title)
+   else:
+      abort(404)
+
+@app.route('/static/img/docs/<path:filename>')
+def static_img_docs(filename):
+   d = os.path.join(app.config['CROSSBARDOCS'], 'static/img/docs')
+   return send_from_directory(d, filename)
+
+@app.route('/static/img/iotcookbook/<path:filename>')
+def static_img_iotcookbook(filename):
+   d = os.path.join(app.config['CROSSBARDOCS'], 'static/img/iotcookbook')
+   return send_from_directory(d, filename)
 
 
 if __name__ == "__main__":
