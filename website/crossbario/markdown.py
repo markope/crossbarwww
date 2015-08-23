0cf60f4d77d448b1200afdc31c72e750fefe84da
Tobias Oberstein <tobias.oberstein@tavendo.de>
2015-08-23 13:47:52 +0200

    Windows fixes; cleanup to 4 spaces indent

--- website/crossbario/markdown.py
+++ website/crossbario/markdown.py
@@ -1,4 +1,5 @@
 # coding=utf8
+# Copyright (C) 2014-2015 Tavendo GmbH. All rights reserved.
 
 import os
 import re
@@ -13,17 +14,17 @@ import mistune
 
 
 def get_git_latest_commit(gitdir = None):
-   """
-   Get git info line on latest commit.
+    """
+    Get git info line on latest commit.
 
-   E.g.: Jay Martin (Wed Aug 13 07:46:39 2014 -0700): Updated Router Realms (markdown)
+    E.g.: Jay Martin (Wed Aug 13 07:46:39 2014 -0700): Updated Router Realms (markdown)
 
-   Usage: env['COMMIT'] = env.GetLatestCommit()
-   """
-   gitdir = gitdir or '.git'
-   cmd = 'git --git-dir="{}" log -1 --pretty=format:"%an (%ad): %s"'.format(gitdir)
-   output = subprocess.check_output(cmd, shell = True).strip()
-   return output
+    Usage: env['COMMIT'] = env.GetLatestCommit()
+    """
+    gitdir = gitdir or '.git'
+    cmd = 'git --git-dir="{}" log -1 --pretty=format:"%an (%ad): %s"'.format(gitdir)
+    output = subprocess.check_output(cmd, shell = True).strip()
+    return output
 
 
 class MyInlineGrammar(mistune.InlineGrammar):
@@ -86,125 +87,114 @@ IMAGE_TEMPLATE = """
 
 class DocPageRenderer(mistune.Renderer):
 
-   def __init__(self, pages, debug = False):
-      mistune.Renderer.__init__(self)
-      self.debug = debug
-      self._pages = pages
-      self._prefix = None
+    def __init__(self, pages, debug = False):
+        mistune.Renderer.__init__(self)
+        self.debug = debug
+        self._pages = pages
+        self._prefix = None
 
-   def header(self, text, level, raw=None):
-      if not HEADER_PAT.match(raw):
-         print("invalid level {} header '{}' (does not match pattern {}): {}".format(level, raw, HEADER_PAT_REGEX, raw))
+    def header(self, text, level, raw=None):
+        if not HEADER_PAT.match(raw):
+            print("invalid level {} header '{}' (does not match pattern {}): {}".format(level, raw, HEADER_PAT_REGEX, raw))
 
-      if text != raw:
-         print("invalid header: {}".format(raw))
+        if text != raw:
+            print("invalid header: {}".format(raw))
 
-      if level > 1: # don't render anchors for main page title
-         # render anchors besides headline elements
-         anchor = text.lower().strip().replace(' ', '-')
-#         res = u"""<h{level} id="{anchor}">{text}<a class="headerlink" title="Permalink to this headline" href="#{anchor}">¶</a></h{level}>""".format(level=level, text=text, anchor=anchor)
-         res = u"""<a name="{anchor}" class="anchor"></a><h{level}>{text}<a class="headerlink" title="Permalink to this headline" href="#{anchor}">¶</a></h{level}>""".format(level=level, text=text, anchor=anchor)
+        if level > 1: # don't render anchors for main page title
+            # render anchors besides headline elements
+            anchor = text.lower().strip().replace(' ', '-')
+            # res = u"""<h{level} id="{anchor}">{text}<a class="headerlink" title="Permalink to this headline" href="#{anchor}">¶</a></h{level}>""".format(level=level, text=text, anchor=anchor)
+            res = u"""<a name="{anchor}" class="anchor"></a><h{level}>{text}<a class="headerlink" title="Permalink to this headline" href="#{anchor}">¶</a></h{level}>""".format(level=level, text=text, anchor=anchor)
 
-         # only render "Goto Top" Links for h2/h3, and not on Home page
-         #if level < 4 and self._prefix is not None:
-         #   # we are rendering the "Goto Top" _before_ a new header
-         #   res += u'<a class="goto-top" href="#top">Goto Top</a>'
-      else:
-         res = u"""<a name="top" class="anchor"></a>""" + mistune.Renderer.header(self, text, level, raw)
+            # only render "Goto Top" Links for h2/h3, and not on Home page
+            #if level < 4 and self._prefix is not None:
+            #   # we are rendering the "Goto Top" _before_ a new header
+            #   res += u'<a class="goto-top" href="#top">Goto Top</a>'
+        else:
+            res = u"""<a name="top" class="anchor"></a>""" + mistune.Renderer.header(self, text, level, raw)
 
-      return res
+        return res
 
-   def hrule(self):
-      return u'<a class="goto-top" href="#top">Top</a>'
+    def hrule(self):
+        return u'<a class="goto-top" href="#top">Top</a>'
 
-   def wiki_link(self, alt, link):
-      if self._prefix:
-         return '<a href="{}/{}">{}</a>'.format(self._prefix, link.replace(' ', '-'), alt)
-      else:
-         return '<a href="{}">{}</a>'.format(link.replace(' ', '-'), alt)
+    def wiki_link(self, alt, link):
+        if self._prefix:
+            return '<a href="{}/{}">{}</a>'.format(self._prefix, link.replace(' ', '-'), alt)
+        else:
+            return '<a href="{}">{}</a>'.format(link.replace(' ', '-'), alt)
 
-   def block_code(self, code, lang):
-      if self.debug:
-         print "CODE", lang, len(code)
+    def block_code(self, code, lang):
+        lexer = None
+        if lang:
+            try:
+                lexer = get_lexer_by_name(lang, stripall = True)
+            except:
+                print("failed to load lexer for language '{}'".format(lang))
 
-      lexer = None
-      if lang:
-         try:
-            lexer = get_lexer_by_name(lang, stripall = True)
-         except:
-            print("failed to load lexer for language '{}'".format(lang))
+        if not lexer:
+            return '\n<pre class="plaincode"><code>{}</code></pre>\n'.format(mistune.escape(code.strip()))
 
-      if not lexer:
-         return '\n<pre class="plaincode"><code>{}</code></pre>\n'.format(mistune.escape(code.strip()))
+        formatter = HtmlFormatter()
+        return '\n<div class="highlight-code">{}</div>\n'.format(highlight(code, lexer, formatter))
 
-      formatter = HtmlFormatter()
-      return '\n<div class="highlight-code">{}</div>\n'.format(highlight(code, lexer, formatter))
+    def autolink(self, link, is_email = False):
+        return mistune.Renderer.autolink(self, link, is_email)
 
-   def autolink(self, link, is_email = False):
-      if self.debug:
-         print "autolink", link
-      return mistune.Renderer.autolink(self, link, is_email)
+    def codespan(self, text):
+        return mistune.Renderer.codespan(self, text)
 
-   def codespan(self, text):
-      if self.debug:
-         print "codespan", text
-      return mistune.Renderer.codespan(self, text)
+    def link(self, link, title, content):
+        if not (link.startswith('http') or link.startswith('/') or link.startswith('#')):
+            if self._prefix:
+                link = "{}/{}/".format(self._prefix, link.replace(' ', '-'))
+            else:
+                link = "{}/".format(link.replace(' ', '-'))
 
-   def link(self, link, title, content):
-      if not (link.startswith('http') or link.startswith('/') or link.startswith('#')):
-         if self._prefix:
-            link = "{}/{}/".format(self._prefix, link.replace(' ', '-'))
-         else:
-            link = "{}/".format(link.replace(' ', '-'))
+        return mistune.Renderer.link(self, link, title, content)
 
-      if self.debug:
-         print "link", link, title, content
-      return mistune.Renderer.link(self, link, title, content)
-
-   def image(self, src, title, alt_text):
-      return IMAGE_TEMPLATE.format(title=(title or alt_text), src=src)
-      #return mistune.Renderer.image(self, src, title, alt_text)
+    def image(self, src, title, alt_text):
+        return IMAGE_TEMPLATE.format(title=(title or alt_text), src=src)
+        #return mistune.Renderer.image(self, src, title, alt_text)
 
 
 class DocPages:
-   def __init__(self, docroot, extensions = ['.md'], debug = False):
-      rend = DocPageRenderer(self, debug)
-      inline = MyInlineLexer(rend)
-      self._renderer = mistune.Markdown(renderer = rend, inline = inline)
-      self._pages = {}
-      self.debug = debug
 
-      total = 0
-      errors = 0
+    def __init__(self, docroot, extensions = ['.md'], debug = False):
+        rend = DocPageRenderer(self, debug)
+        inline = MyInlineLexer(rend)
+        self._renderer = mistune.Markdown(renderer = rend, inline = inline)
+        self._pages = {}
+        self.debug = debug
 
-      for dirpath, dirnames, filenames in os.walk(docroot):
-         dirpath_rel = os.path.relpath(dirpath, docroot)
-         for f in filenames:
-            base, ext = os.path.splitext(f)
-            if ext in extensions:
-               total += 1
-               fp = os.path.join(dirpath, f)
-               with open(fp, 'r') as fd:
-                  source = fd.read()
-                  try:
-                     if self.debug:
-                        print "\nprocessing {}".format(fp)
-                     if base == 'Home':
-                        rend._prefix = None
-                     else:
-                        rend._prefix = '..'
-                     contents = self._renderer(source)
-                  except Exception as e:
-                     print "warning: failed to process {}: {}".format(fp, e)
-                     errors += 1
-                  else:
-                     path = base
-                     #path = os.path.join(dirpath_rel, base)
-                     self._pages[path] = contents
-      print("processed {} files: {} ok, {} error".format(total, len(self._pages), errors))
+        total = 0
+        errors = 0
 
-      #self._pages[None] = self._pages[index]
+        for dirpath, dirnames, filenames in os.walk(docroot):
+            dirpath_rel = os.path.relpath(dirpath, docroot)
+            for f in filenames:
+                base, ext = os.path.splitext(f)
+                if ext in extensions:
+                    total += 1
+                    fp = os.path.join(dirpath, f)
+                with open(fp, 'r') as fd:
+                    source = fd.read()
+                    try:
+                        if self.debug:
+                            print "\nprocessing {}".format(fp)
+                        if base == 'Home':
+                            rend._prefix = None
+                        else:
+                            rend._prefix = '..'
+                        contents = self._renderer(source)
+                    except Exception as e:
+                        print "warning: failed to process {}: {}".format(fp, e)
+                        errors += 1
+                    else:
+                        path = base
+                        #path = os.path.join(dirpath_rel, base)
+                        self._pages[path] = contents
+        print("processed {} files: {} ok, {} error".format(total, len(self._pages), errors))
 
-   def render(self, path):
-      return self._pages.get(path, None)
-
+    def render(self, path):
+        return self._pages.get(path, None)
--- website/crossbario/server.py
+++ website/crossbario/server.py
@@ -1,3 +1,6 @@
+# coding=utf8
+# Copyright (C) 2014-2015 Tavendo GmbH. All rights reserved.
+
 import os
 import re
 import sys
@@ -20,21 +23,23 @@ from markdown import DocPages, get_git_latest_commit
 mimetypes.add_type('image/svg+xml', '.svg')
 mimetypes.add_type('text/javascript', '.jgz')
 
+
 # Jinja2 extension for Pygments
 #
 # Note: To generate Pygments CSS file for style: pygmentize -S default -f html > pygments.css
 #
 try:
-   import jinja2_highlight
+    import jinja2_highlight
 except ImportError:
-   print("Warning: Jinja2-Highlight not available")
-   SiteFlask = Flask
-   HAS_HIGHLIGHT = False
+    print("Warning: Jinja2-Highlight not available")
+    SiteFlask = Flask
+    HAS_HIGHLIGHT = False
 else:
-   class SiteFlask(Flask):
-      jinja_options = dict(Flask.jinja_options)
-      jinja_options.setdefault('extensions',[]).append('jinja2_highlight.HighlightExtension')
-   HAS_HIGHLIGHT = True
+    class SiteFlask(Flask):
+        jinja_options = dict(Flask.jinja_options)
+        jinja_options.setdefault('extensions',[]).append('jinja2_highlight.HighlightExtension')
+    HAS_HIGHLIGHT = True
+
 
 # Main app object
 #
@@ -43,64 +48,69 @@ app.secret_key = str(uuid.uuid4())
 app.config['DEBUG'] = True
 app.config['CROSSBARDOCS'] = os.path.abspath('../../../crossbardocs')
 
-d = os.path.join(app.config['CROSSBARDOCS'], 'pages/docs')
+d = os.path.join(app.config['CROSSBARDOCS'], 'pages', 'docs')
 print("Processsing Markdown pages from {} ..".format(d))
 app.pages_docs = DocPages(d)
 
-d = os.path.join(app.config['CROSSBARDOCS'], 'pages/iotcookbook')
+d = os.path.join(app.config['CROSSBARDOCS'], 'pages', 'iotcookbook')
 print("Processsing Markdown pages from {} ..".format(d))
 app.pages_iotcookbook = DocPages(d)
+
 
 # Routes
 #
 @app.route('/')
 def page_home():
-   session['site_area'] = 'landing'
-   return render_template('index.html')
+    session['site_area'] = 'landing'
+    return render_template('index.html')
+
 
 @app.route('/docs/<path:path>/')
 @app.route('/docs/')
 def page_docs(path=None):
-   session['site_area'] = 'docs'
-   if path is None or path.strip() == "":
-      title = 'Documentation'
-      path = 'Home'
-   else:
-      title = path.replace('-', ' ')
+    session['site_area'] = 'docs'
+    if path is None or path.strip() == "":
+        title = 'Documentation'
+        path = 'Home'
+    else:
+        title = path.replace('-', ' ')
 
-   contents = app.pages_docs.render(path)
+    contents = app.pages_docs.render(path)
 
-   if contents:
-      return render_template('page_docs.html', contents=contents, title=title)
-   else:
-      abort(404)
+    if contents:
+        return render_template('page_docs.html', contents=contents, title=title)
+    else:
+        abort(404)
+
 
 @app.route('/iotcookbook/<path:path>/')
 @app.route('/iotcookbook/')
 def page_iotcookbook(path=None):
-   session['site_area'] = 'iotcookbook'
-   if path is None or path.strip() == "":
-      title = 'IoT Cookbook'
-      path = 'Home'
-   else:
-      title = path.replace('-', ' ')
+    session['site_area'] = 'iotcookbook'
+    if path is None or path.strip() == "":
+        title = 'IoT Cookbook'
+        path = 'Home'
+    else:
+        title = path.replace('-', ' ')
 
-   contents = app.pages_iotcookbook.render(path)
+    contents = app.pages_iotcookbook.render(path)
 
-   if contents:
-      return render_template('page_iotcookbook.html', contents=contents, title=title)
-   else:
-      abort(404)
+    if contents:
+        return render_template('page_iotcookbook.html', contents=contents, title=title)
+    else:
+        abort(404)
+
 
 @app.route('/static/img/docs/<path:filename>')
 def static_img_docs(filename):
-   d = os.path.join(app.config['CROSSBARDOCS'], 'static/img/docs')
-   return send_from_directory(d, filename)
+    d = os.path.join(app.config['CROSSBARDOCS'], 'static', 'img', 'docs')
+    return send_from_directory(d, filename)
+
 
 @app.route('/static/img/iotcookbook/<path:filename>')
 def static_img_iotcookbook(filename):
-   d = os.path.join(app.config['CROSSBARDOCS'], 'static/img/iotcookbook')
-   return send_from_directory(d, filename)
+    d = os.path.join(app.config['CROSSBARDOCS'], 'static', 'img', 'iotcookbook')
+    return send_from_directory(d, filename)
 
 
 if __name__ == "__main__":
